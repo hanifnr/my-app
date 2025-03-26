69323b9ad8be388403e37bfb66e7884badf8e6e3
hanif.nr <hanif.nr11@gmail.com>
2025-03-26 10:43:13 +0900

    init commit

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import cors from 'cors';
+import * as dotenv from 'dotenv';
+import pool from './utils/database';
+
+dotenv.config();
+
+const app = express();
+const PORT = parseInt(process.env.PORT as string, 10) || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: "Hello from EC2 + Docker!" });
+});
+
+app.get('/users', async (req, res) => {
+    const { rows } = await pool.query('SELECT * FROM users');
+    res.json(rows);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ src/utils/database.ts
@@ -0,0 +1,18 @@
+import { Pool } from 'pg';
+import dotenv from 'dotenv';
+
+// Load environment variables
+dotenv.config();
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: Number(process.env.DB_PORT),
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
+export default pool;
